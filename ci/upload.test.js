a3c7a3625cfb40695aba044266583ac23a79cdf0
Henning Kvinnesland <henning.kvinnesland@bouvet.no>
2018-08-19 19:46:21 +0200

    Upload all the things.

--- ci/upload.test.js
+++ ci/upload.test.js
@@ -1,37 +1,68 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const puppeteer = require('puppeteer');
+const {watch, statSync, readdirSync} = require('fs');
+const max = require ('lodash.max');
 
 const timeout = 60 * 1000 * 120;
 jasmine.DEFAULT_TIMEOUT_INTERVAL = timeout;
+const fileCount = () => readdirSync('./demos').length;
+
+let currentCount = fileCount();
 
 /** @type {Browser} browser */
 let browser;
 /** @type {Page} page */
 let page;
 
+
 const getLastReplay = () => {
-  return 'demos/F9695E1D4EFDDC44A4994889B74C7E14.replay';
+  const files = readdirSync('./demos');
+
+  const latest = max(files, (f) => {
+    const fullpath = path.join(dir, f);
+    return statSync(fullpath).ctime;
+  });
+
+  return `demos/${latest}`
+};
+
+const monitorShare = async (callback) => {
+  const count = fileCount();
+
+  console.log(`Current: ${currentCount}`);
+  console.log(`New; ${count}`);
+
+  if (count !== currentCount) {
+    await callback();
+    currentCount = count;
+  }
+
+  setTimeout(() => monitorShare(callback), 5000);
 };
 
 beforeAll(async () => {
   browser = await puppeteer.launch({headless: false, userDataDir: './userProfile'});
   page = await browser.newPage();
-
-  await page.goto('https://ballchasing.com/upload');
   await page.setViewport({width: 1600, height: 768});
+
 });
 
-describe('Upload a demo', () => {
-  test('Upload latest file.', async () => {
-    // Private please.
-    const radioSelector ='input[name="visibility"]';
-    await page.waitFor(radioSelector, 3000);
-    await page.evaluate(`$('${radioSelector}').click();`);
+doUpload = async () => {
+  // Private please.
+  await page.goto('https://ballchasing.com/upload');
+  const radioSelector ='input[name="visibility"]';
+  await page.waitFor(radioSelector, 3000);
+  await page.evaluate(`$('${radioSelector}').click();`);
 
-    const uploadInput = await page.$('#replay-upload');
-    await uploadInput.uploadFile(getLastReplay());
-    // Need to increase timeout above 30s
-    await page.waitFor('#lol', 50000);
+  const uploadInput = await page.$('#replay-upload');
+  await uploadInput.uploadFile(getLastReplay());
+  // Need to increase timeout above 30s
+};
+
+describe('Upload new demos', () => {
+  test('Start watcher', async () => {
+    await monitorShare(doUpload);
+    await page.waitFor('#lol', {timeout});
   });
 });
 
